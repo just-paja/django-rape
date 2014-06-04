813715e67b859fdc13082b1b640b5cb9f481e865
just-paja <pavel@proboston.net>
2014-06-04 14:08:41 +0200

    Template tags, accept request as param

--- rape/templatetags/rape.py
+++ rape/templatetags/rape.py
@@ -5,24 +5,24 @@ from .. import helpers, settings
 register = template.Library()
 
 
-def rape_url(url):
-	return helpers.rape_static_url(url)
+def rape_url(url, request=None):
+	return helpers.rape_static_url(url, request)
 
 
-def raped_script(name):
-	return reverse('raped_script', args=[settings.RAPE_SERIAL, name])
+def raped_script(name, request=None):
+	return helpers.add_host(reverse('raped_script', args=[settings.RAPE_SERIAL, name]), request)
 
 
-def raped_style(name):
-	return reverse('raped_style', args=[settings.RAPE_SERIAL, name])
+def raped_style(name, request=None):
+	return helpers.add_host(reverse('raped_style', args=[settings.RAPE_SERIAL, name]), request)
 
 
-def tag_script(name):
-	return '<script type="text/javascript" src="%s"></script>' % (raped_script(name))
+def tag_script(name, request=None):
+	return '<script type="text/javascript" src="%s"></script>' % (raped_script(name, request))
 
 
-def tag_style(name):
-	return '<link rel="stylesheet" type="text/css" href="%s">' % (raped_style(name))
+def tag_style(name, request=None):
+	return '<link rel="stylesheet" type="text/css" href="%s">' % (raped_style(name, request))
 
 
 def rape_static_url(url, request=None):
