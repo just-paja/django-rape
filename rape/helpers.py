95a39a1a14653f40ceed9282fb7a94420a5de8d6
just-paja <pavel@proboston.net>
2014-07-30 11:54:47 +0200

    More inline docs

--- rape/helpers.py
+++ rape/helpers.py
@@ -119,14 +119,25 @@ def checkout_file(res_type, fp):
 
 
 
+"""
+Has this predefined resource changed since its' last compilation?
+
+* request  request
+* res_type string  Resource type ('script', 'style')
+* name     string Name of the resource defined in settings.RAPED_(STYLES|SCRIPTS)
+
+Returns bool
+"""
 def changed(request, res_type, name):
 	file_name = get_output_path(request, res_type, name)
 	changed = False
 
+	# Check if the resource is compiled
 	if os.path.isfile(file_name):
 		last_change = os.path.getmtime(file_name)
 		file_list = get_file_list(res_type, name)
 
+		# Walk trough all files from this resource
 		for res_name in file_list:
 			res_path = res_name
 			res_last_change = os.path.getmtime(res_path)
@@ -135,16 +146,22 @@ def changed(request, res_type, name):
 				changed = True
 				break
 
+	# It is not compiled, so we can act as it has changed
 	else:
 		changed = True
 
 	return changed
 
 
-def get_res_name(res_type, res_name):
-	return '%s/%s.%s' % (settings.RAPE_PATH, res_name, get_postfix(res_type, True))
+"""
+Get name of raped resource - output file
 
+* request  request
+* res_type string  Resource type ('script', 'style')
+* name     string Name of the resource defined in settings.RAPED_(STYLES|SCRIPTS)
 
+Returns string, a relative path from static directory
+"""
 def get_output_name(request, res_type, name):
 	protocol = 'http'
 	arg = 'plain'
@@ -155,14 +172,39 @@ def get_output_name(request, res_type, name):
 	return 'rape/%s/%s/%s-%s-%s.%s.%s' % (res_type, name, protocol, request.META['HTTP_HOST'], arg, settings.RAPE_SERIAL, get_postfix(res_type))
 
 
+"""
+Get path for raped resource
+
+* request  request
+* res_type string  Resource type ('script', 'style')
+* name     string Name of the resource defined in settings.RAPED_(STYLES|SCRIPTS)
+
+Returns string, an absolute path to static directory
+"""
 def get_output_path(request, res_type, name):
 	return '%s/%s' % (settings.STATIC_ROOT, get_output_name(request, res_type, name))
 
 
+"""
+Get absolute URL for raped resource
+
+* res_type string  Resource type ('script', 'style')
+* name     string Name of the resource defined in settings.RAPED_(STYLES|SCRIPTS)
+
+Returns string, an absolute path from domain root excluding host
+"""
 def get_output_url(res_type, name):
 	return '%s%s' % (settings.STATIC_URL, get_output_name(res_type, name))
 
 
+"""
+Get postfix for resource type
+
+* res_type string  Resource type ('script', 'style')
+* home     bool    Should it be postfix that it's compiled from?
+
+Returns string, an absolute path to static directory
+"""
 def get_postfix(res_type, home=False):
 	postfix = 'js'
 	if res_type == 'style':
